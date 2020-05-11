3b36ecdaf925c51279172ff08fb325feeb9be30a
Leeleo3x <leeleo3x@gmail.com>
2020-05-11 11:50:50 -0700

    update.

--- src/core/store.rkt
+++ src/core/store.rkt
@@ -6,20 +6,34 @@
 (define-generics cell 
                  (cell-size cell)
                  (cell-offset cell)
-                 (cell-value cell))
+                 (cell-value cell)
+                 (set-cell-value! cell value))
 
 (define (mcell-size mcell) 1)
 
-(struct mcell (offset value) #:transparent
+(struct mcell (offset [value #:mutable]) #:transparent
   #:methods gen:cell
   [(define cell-size mcell-size)
-   (define cell-offset mcell-offset)
-   (define cell-value mcell-value)])
+   (define (cell-offset cell) mcell-offset)
+   (define (cell-value cell) mcell-value)
+   (define (set-cell-value! cell value) set-mcell-value!)])
 
 (define (scell-size scell) 32)
 
-(struct scell (offset value) #:transparent
+(struct scell (offset [value #:mutable]) #:transparent
   #:methods gen:cell
   [(define cell-size scell-size)
-   (define cell-offset scell-offset)
-   (define cell-value scell-value)])
+   (define (cell-offset cell) scell-offset)
+   (define (cell-value cell) scell-value)])
+
+(struct store (cell [ls #:mutable]) #:transparent)
+
+(define (find-or-create-cell s offset) 
+ (define ls (store-ls s))
+ (define result (findf (lambda (c) (equal? offset (cell-offset c))) ls))
+ (cond 
+  [(equal? result #f) 
+   (define c ((store-cell s) offset 0)) 
+   (set-store-ls! s (append ls (list c)))
+   c]
+  [else result]))
