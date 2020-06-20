0df10cb38ca787e32d7b112b4be84b1421e9faac
Leeleo3x <leeleo3x@gmail.com>
2020-06-20 12:21:52 -0700

    update.

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection 'multi)
+(define deps '("base"))
+(define build-deps '("rosette" "binaryio" "megaparsack" "scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/sonic-test.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(leo))
---
+++ scribblings/sonic-test.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[sonic
+                    racket/base]]
+
+@title{sonic}
+@author{leo}
+
+@defmodule[sonic]
+
+Package Description Here
